aaa3a0eb6e13410fe0fa57dc7c2f4ca86f878009
Lokesh Surana <lokesh8203@gmail.com>
2023-04-08 17:18:52 +0530

    scrape - all financial data from last 5 years

--- Banks/Balancesheets with Additional parameters Version 2/scrape.py
+++ Banks/Balancesheets with Additional parameters Version 2/scrape.py
@@ -4,10 +4,7 @@ import os
 from bs4 import BeautifulSoup
 
 #popluate the list of stock symbols with Banks from BAnknifty nse website
-
 # stock_symbols = ['ICICIBANK', 'HDFCBANK', 'KOTAKBANK', 'AXISBANK', 'INDUSINDBK', 'BANDHANBNK', 'BANKBARODA', 'BANKINDIA', 'CANBK', 'FEDERALBNK', 'IDBI', 'IDFCFIRSTB', 'PNB', 'RBLBANK', 'SBIN', 'YESBANK']
-
-#We need to get financial data from moneycontrol website
 
 stock_symbol = 'hdfcbank'
 ratio_url = 'https://www.moneycontrol.com/financials/hdfcbank/ratiosVI/HDF01'
@@ -36,10 +33,8 @@ for row in rows:
     #write to csv file
     csv_writer.writerow(cols)
 
-balancesheet_url_page2 = 'https://www.moneycontrol.com/financials/hdfcbank/balance-sheetVI/HDF01/2#HDF01'
-
-#Append data from page two to the same csv file
-r = requests.get(balancesheet_url_page2)
+#use ratio url
+r = requests.get(ratio_url)
 htmlContent = r.content
 soup = BeautifulSoup(htmlContent, 'html.parser')
 
@@ -47,29 +42,46 @@ table = soup.find('table', attrs={'class':'mctable1'})
 #find all rows
 rows = table.find_all('tr')
 for row in rows:
-    #find the same named row from previous csv file
-    #traverse thorugh csv file and compare the first element of each row
-    #if the first element of each row is same, then append the rest of the elements to the same row
-    # print(filename)
-    csv_traverse = csv.reader(open(filename, 'r'))
+    #get columns for each row
+    cols = row.find_all('td')
+    #get text from each column
+    cols = [ele.text.strip() for ele in cols]
+    #write to csv file
+    csv_writer.writerow(cols)
+
+#use PNL url
+r = requests.get(pnl_url)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
+
+table = soup.find('table', attrs={'class':'mctable1'})
+#find all rows
+rows = table.find_all('tr')
+for row in rows:
+    #get columns for each row
+    cols = row.find_all('td')
+    #get text from each column
+    cols = [ele.text.strip() for ele in cols]
+    #write to csv file
+    csv_writer.writerow(cols)
+
+
+#use yearly results url
+r = requests.get(yearly_results_url)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
+
+table = soup.find('table', attrs={'class':'mctable1'})
+#find all rows
+rows = table.find_all('tr')
+for row in rows:
+    #get columns for each row
+    cols = row.find_all('td')
+    #get text from each column
+    cols = [ele.text.strip() for ele in cols]
+    #write to csv file
+    csv_writer.writerow(cols)
     
-    for csv_row in csv_traverse:
-        #if csv_row had not element then skip
-        if(len(csv_row) == 0):
-            continue
-        elif (csv_row[0] == row.find('td').text.strip()):
-            print('I')
-            #get columns for each row
-            cols = row.find_all('td')
-            #get text from each column
-            cols = [ele.text.strip() for ele in cols]
-            #write to csv file
-            print(cols)
-            csv_writer.writerow(cols)
-        #move to next row
-        #     
-
-
 #loop through the list of stock symbols
 # for stock_symbol in stock_symbols:
 #     url = 'https://www.screener.in/company/' + stock_symbol + '/consolidated/#balance-sheet'
