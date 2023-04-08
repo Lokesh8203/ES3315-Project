e0f40191f2873cd7a0e1216e6983db746e2a73c0
Lokesh Surana <lokesh8203@gmail.com>
2023-04-08 17:25:26 +0530

    scrape2 - last 10 yers data

--- Banks/Balancesheets with Additional parameters Version 2/scrape2.py
+++ Banks/Balancesheets with Additional parameters Version 2/scrape2.py
@@ -43,7 +43,6 @@ for row in rows:
         if(len(csv_row) == 0):
             continue
         elif (csv_row[0] == row.find('td').text.strip()):
-            print('I')
             #get columns for each row
             cols = row.find_all('td')
             #get text from each column
@@ -53,7 +52,95 @@ for row in rows:
             cols.pop(0)
             csv_writer.writerow(csv_row + cols)
 
+ration_url_page2 = 'https://www.moneycontrol.com/financials/hdfcbank/ratiosVI/HDF01/2#HDF01'
 
+#Append data from page two to the same csv file
+r = requests.get(ration_url_page2)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
 
-
+table = soup.find('table', attrs={'class':'mctable1'})
+#find all rows
+rows = table.find_all('tr')
+for row in rows:
+    #find the same named row from previous csv file
+    #traverse thorugh csv file and compare the first element of each row
+    #if the first element of each row is same, then append the rest of the elements to the same row
+    # print(filename)
+    csv_traverse = csv.reader(open(filename2, 'r'))
+    
+    for csv_row in csv_traverse:
+        #if csv_row had not element then skip
+        if(len(csv_row) == 0):
+            continue
+        elif (csv_row[0] == row.find('td').text.strip()):
+            #get columns for each row
+            cols = row.find_all('td')
+            #get text from each column
+            cols = [ele.text.strip() for ele in cols]
+            #write to csv file
+            #remove the first element from the list
+            cols.pop(0)
+            csv_writer.writerow(csv_row + cols)
         
+pnl_url_page2 = 'https://www.moneycontrol.com/financials/hdfcbank/profit-lossVI/HDF01/2#HDF01'
+
+#Append data from page two to the same csv file
+r = requests.get(pnl_url_page2)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
+
+table = soup.find('table', attrs={'class':'mctable1'})
+#find all rows
+rows = table.find_all('tr')
+for row in rows:
+    #find the same named row from previous csv file
+    #traverse thorugh csv file and compare the first element of each row
+    #if the first element of each row is same, then append the rest of the elements to the same row
+    # print(filename)
+    csv_traverse = csv.reader(open(filename2, 'r'))
+    
+    for csv_row in csv_traverse:
+        #if csv_row had not element then skip
+        if(len(csv_row) == 0):
+            continue
+        elif (csv_row[0] == row.find('td').text.strip()):
+            #get columns for each row
+            cols = row.find_all('td')
+            #get text from each column
+            cols = [ele.text.strip() for ele in cols]
+            #write to csv file
+            #remove the first element from the list
+            cols.pop(0)
+            csv_writer.writerow(csv_row + cols)
+
+yearly_results_url_page2 = 'https://www.moneycontrol.com/financials/hdfcbank/consolidated-results/yearly/HDF01/2#HDF01'
+
+#Append data from page two to the same csv file
+r = requests.get(yearly_results_url_page2)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
+
+table = soup.find('table', attrs={'class':'mctable1'})
+#find all rows
+rows = table.find_all('tr')
+for row in rows:
+    #find the same named row from previous csv file
+    #traverse thorugh csv file and compare the first element of each row
+    #if the first element of each row is same, then append the rest of the elements to the same row
+    # print(filename)
+    csv_traverse = csv.reader(open(filename2, 'r'))
+    
+    for csv_row in csv_traverse:
+        #if csv_row had not element then skip
+        if(len(csv_row) == 0):
+            continue
+        elif (csv_row[0] == row.find('td').text.strip()):
+            #get columns for each row
+            cols = row.find_all('td')
+            #get text from each column
+            cols = [ele.text.strip() for ele in cols]
+            #write to csv file
+            #remove the first element from the list
+            cols.pop(0)
+            csv_writer.writerow(csv_row + cols)
